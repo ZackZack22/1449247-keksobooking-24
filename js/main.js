d43c4f448e3f44c8c4ba3a282f0d870c6063af4d
ZackSamadov <samadov.zakir@bk.ru>
2021-10-25 16:45:55 +0400

    Задание #5

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+console.log(getRandomInt(5));
+console.log(getRandomInt(1));
+console.log(Math.random());
+
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random источник
